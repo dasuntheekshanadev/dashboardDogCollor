401d2ece7536ac3b2983cb26f0e06f6344706e6e
Dasun Theekshana <40088028+dasuntheekshanadev@users.noreply.github.com>
2024-04-05 00:35:55 +0530

    config

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,12 +1,19 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDovvRiMrVNTuIJVt54e-duROvWK82mpPk",
-    authDomain: "iotfinal-f0658.firebaseapp.com",
-    databaseURL: "https://iotfinal-f0658-default-rtdb.firebaseio.com",
-    projectId: "iotfinal-f0658",
-    storageBucket: "iotfinal-f0658.appspot.com",
-    messagingSenderId: "836429777085",
-    appId: "1:836429777085:web:ff536891e662e494740fd4",
-    measurementId: "G-76S95JWWJB"
-  };
+  apiKey: "AIzaSyAj0uD_zYCrMP1dddFyAClbmudFD7HZhNM",
+
+  authDomain: "iotproj-63326.firebaseapp.com",
+
+  databaseURL: "https://iotfinal-f0658-default-rtdb.firebaseio.com",
+
+  projectId: "iotproj-63326",
+
+  storageBucket: "iotproj-63326.appspot.com",
+
+  messagingSenderId: "90274505071",
+
+  appId: "1:90274505071:web:e40563e057a29d2d64c89a",
+
+  measurementId: "G-T7X6FWKVFD",
+};
 
 export default firebaseConfig;
